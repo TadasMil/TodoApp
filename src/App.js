663234d9d5758e21723a2ce7233237c57bccc6c5
tadas <ttadas10@gmail.com>
2020-07-06 12:48:13 +0300

    Completed the structure of App and TodoComponent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
+import TodoComponent from './TodoComponent';
 
 function App() {
+
   return (
     <div>
-      <div className="container">
-        <div className="row">
-           <TodoInput></TodoInput>
-            <TodoList></TodoList>
-        </div>
-      </div>
+      <TodoComponent/>
     </div>
   );
 }
---
+++ src/TodoComponent.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+export default class TodoComponent extends Component {
+    state = {
+        items: [{id: 1, title: 'wake up'},
+                {id: 2, title: 'Make breakfast'},
+              ],
+        id: uuidv4(), //Using uuid package to generate new ID
+        item: '',
+        editItem: false
+      };
+
+      handleChange = (e) => {
+        console.log('handle change')
+      }
+      handleSubmit = (e) => {
+        console.log('handle submit')
+      }
+      clearList = () => {
+        console.log('handle clear')
+      }
+      handleSingleDelete = (id) => {
+        console.log(`handle single delete ${id}`)
+      }
+      handleEdit = (id) => {
+        console.log(`handle edit ${id}`)
+      }
+    
+    render() {
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className=".col-10 mx-auto col-md-8 mt-5">
+                        <h3 className="text-capitalize text-center">
+                        Pridėti nauja įrašą:
+                        </h3>
+                        <TodoInput item={this.state.item}
+                        editItem={this.state.editItem}
+                        handleChange={this.handleChange}
+                        handleSubmit={this.handleSubmit}/>
+                        <TodoList items={this.state.items}
+                        clearList={this.clearList}
+                        handleSingleDelete={this.handleSingleDelete}
+                        editItem={this.handleEdit}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
